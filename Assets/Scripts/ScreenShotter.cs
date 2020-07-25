a1be61ce364cb8342e4a23e948ddaada50370214
Sorin <jordache_sor@yahoo.com>
2020-07-25 14:29:34 +0300

    added screenshot script

---
+++ Assets/Scripts/ScreenShotter.cs
@@ -0,0 +1,28 @@
+﻿#if (UNITY_EDITOR) 
+using System;
+using System.Globalization;
+using System.IO;
+using UnityEngine;
+
+public class ScreenShotter : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            TakeScreenshot();
+        }
+    }
+
+    public void TakeScreenshot()
+    {
+        string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "screenshots", Application.productName);
+        if (!Directory.Exists(folder))
+        {
+            Directory.CreateDirectory(folder);
+        }
+        string fileName = Application.productName + " screenshot " + DateTime.Now.ToString("HH-mm-ss-ffff", CultureInfo.InvariantCulture) + ".png";
+        ScreenCapture.CaptureScreenshot(Path.Combine(folder, fileName));
+    }
+}
+#endif
